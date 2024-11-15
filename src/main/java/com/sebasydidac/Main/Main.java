7b0c2e02ac43c3091af87742d4954425087a3952
SebastianARG <sebastianarengifog16@gmail.com>
2024-11-15 15:24:29 +0100

    Changes by rodri

--- src/main/java/com/sebasydidac/Main/Main.java
+++ src/main/java/com/sebasydidac/Main/Main.java
@@ -2,6 +2,8 @@ package com.sebasydidac.Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Hello, Sebas!");
+        System.out.println("Debajo el mar! Tututu tututu tutu");
     }
 }
