1e935f80a6863ee5b50c47c822890dbd6ad044c0
SebastianARG <sebastianarengifog16@gmail.com>
2024-11-22 17:36:29 +0100

    cosas acomodadas

--- src/main/java/com/sebasydidac/Main/Main.java
+++ src/main/java/com/sebasydidac/Main/Main.java
@@ -3,6 +3,7 @@ import com.sebasydidac.Logica.Tablero;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tablero tablero = new Tablero();
+        tablero.imprimirTablero();
     }
 }
